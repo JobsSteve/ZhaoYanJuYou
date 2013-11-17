d4511fd24a96acd98f30c56e78305234dab12826
yuri <xyljxc@126.com>
2013-11-17 22:36:05 +0800

    1117_badge

--- src/com/zhaoyan/juyou/fragment/GuanJiaFragment.java
+++ src/com/zhaoyan/juyou/fragment/GuanJiaFragment.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.zhaoyan.common.util.Log;
+import com.zhaoyan.common.view.BadgeView;
 import com.zhaoyan.juyou.R;
 import com.zhaoyan.juyou.activity.AudioActivity;
 import com.zhaoyan.juyou.activity.HistoryActivity;
@@ -17,6 +20,7 @@ public class GuanJiaFragment extends BaseFragment implements OnClickListener {
 	//items
 	private View mMusicView;
 	private View mHistoryView;
+	private BadgeView badgeView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,12 +31,24 @@ public class GuanJiaFragment extends BaseFragment implements OnClickListener {
 		return rootView;
 	}
 	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d(TAG, "onResume()");
+	}
+	
 	public void initView(View view){
 		mMusicView = view.findViewById(R.id.rl_guanjia_music);
 		mMusicView.setOnClickListener(this);
 		
 		mHistoryView = view.findViewById(R.id.rl_guanjia_history);
 		mHistoryView.setOnClickListener(this);
+		
+		View imageView = view.findViewById(R.id.iv_guanjia_history);
+		badgeView = new BadgeView(mContext, imageView);
+		badgeView.setText("12");
+		badgeView.show();
 	}
 
 	@Override
