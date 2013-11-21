7e867993ad05b54c0c1cafc2946a3ecc225d2434
alex_liang <liangxiaofei1100@163.com>
2013-11-21 16:58:29 +0800

    20131121 Modify ClassifyActivity, use new scan method.

--- src/com/zhaoyan/juyou/activity/ClassifyActivity.java
+++ src/com/zhaoyan/juyou/activity/ClassifyActivity.java
@@ -1,47 +1,44 @@
 package com.zhaoyan.juyou.activity;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
-import android.R.integer;
-import android.content.Intent;
-import android.content.SharedPreferences.Editor;
-import android.os.AsyncTask;
-import android.os.AsyncTask.Status;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.AbsListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.zhaoyan.common.file.FileManager;
 import com.zhaoyan.common.util.Log;
 import com.zhaoyan.juyou.R;
 import com.zhaoyan.juyou.adapter.FileInfoAdapter;
 import com.zhaoyan.juyou.adapter.FileInfoAdapter.ViewHolder;
 import com.zhaoyan.juyou.common.ActionMenu;
 import com.zhaoyan.juyou.common.ActionMenu.ActionMenuItem;
-import com.zhaoyan.juyou.common.FileTransferUtil.TransportCallback;
+import com.zhaoyan.juyou.common.FileClassifyScanner;
+import com.zhaoyan.juyou.common.FileClassifyScanner.FileClassifyScanListener;
 import com.zhaoyan.juyou.common.FileInfo;
 import com.zhaoyan.juyou.common.FileInfoManager;
 import com.zhaoyan.juyou.common.FileTransferUtil;
+import com.zhaoyan.juyou.common.FileTransferUtil.TransportCallback;
 import com.zhaoyan.juyou.common.MenuTabManager;
 import com.zhaoyan.juyou.common.MenuTabManager.onMenuItemClickListener;
 import com.zhaoyan.juyou.common.ZYConstant;
 
-public class ClassifyActivity extends BaseActivity implements OnItemClickListener, OnItemLongClickListener, OnScrollListener, onMenuItemClickListener {
+public class ClassifyActivity extends BaseActivity implements
+		OnItemClickListener, OnItemLongClickListener, OnScrollListener,
+		onMenuItemClickListener, FileClassifyScanListener {
 	private static final String TAG = "ClassifyActivity";
 	protected ProgressBar mLoadingBar;
 	private ListView mListView;
@@ -50,7 +47,8 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 	private List<FileInfo> mTestList = new ArrayList<FileInfo>();
 	protected FileInfoAdapter mAdapter;
 	protected FileInfoManager mFileInfoManager;
-	
+	private FileClassifyScanner mFileClassifyScanner;
+
 	private LinearLayout mMenuHolder;
 	private View mMenuBarView;
 	private MenuTabManager mMenuTabManager;
@@ -62,149 +60,119 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 	public static final String CLASSIFY_TYPE = "CLASSIFY_TYPE";
 	private int mType = -1;
 	private String[] filterType = null;
-	
-	private GetFileTask mGetFileTask = null;
 
-	private static final int MSG_UPDATE_UI = 0;
+	private static final int MSG_SCAN_START = 1;
+	private static final int MSG_SCAN_COMPLETE = 2;
+
 	private Handler mHandler = new Handler() {
-		public void handleMessage(android.os.Message msg) {
-			mLoadingBar.setVisibility(View.INVISIBLE);
-			mTipView.setVisibility(View.INVISIBLE);
-			
-			try {
-				Collections.sort(mTestList, FileInfo.DATE_COMPARATOR);
-			} catch (Exception e) {
-				Log.e(TAG, "handleMessage:" + e.toString());
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case MSG_SCAN_START:
+				mLoadingBar.setVisibility(View.VISIBLE);
+				mTipView.setVisibility(View.VISIBLE);
+				mTipView.setText(R.string.file_classify_loading);
+				break;
+			case MSG_SCAN_COMPLETE:
+				mLoadingBar.setVisibility(View.INVISIBLE);
+				mTipView.setVisibility(View.INVISIBLE);
+
+				Vector<FileInfo> fileInfos = (Vector<FileInfo>) msg.obj;
+				mAdapter.setList(fileInfos);
+				mAdapter.selectAll(false);
+				mAdapter.notifyDataSetChanged();
+				updateTitleNum(-1, mTestList.size());
+				break;
+
+			default:
+				break;
 			}
-			mAdapter.setList(mTestList);
-			mAdapter.selectAll(false);
-			mAdapter.notifyDataSetChanged();
-			updateTitleNum(-1, mTestList.size());
+
 		};
 	};
 
+	@Override
 	protected void onCreate(android.os.Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.classify_main);
-		
+
 		Bundle bundle = getIntent().getExtras();
 		mType = bundle.getInt(CLASSIFY_TYPE);
 		if (TYPE_DOC == mType) {
 			initTitle(R.string.file_document);
 			filterType = getResources().getStringArray(R.array.doc_file);
-		}else if (TYPE_ARCHIVE == mType) {
+		} else if (TYPE_ARCHIVE == mType) {
 			initTitle(R.string.file_compressed);
 			filterType = getResources().getStringArray(R.array.archive_file);
-		}else if (TYPE_APK == mType) {
+		} else if (TYPE_APK == mType) {
 			initTitle(R.string.file_apk);
 			filterType = getResources().getStringArray(R.array.apk_file);
 		}
 		setTitleNumVisible(true);
-		
+
 		mListView = (ListView) findViewById(R.id.lv_classify);
 		mListView.setOnItemClickListener(this);
 		mListView.setOnItemLongClickListener(this);
 		mListView.setOnScrollListener(this);
-		
+
 		mTipView = (TextView) findViewById(R.id.tv_classify_tip);
 		mLoadingBar = (ProgressBar) findViewById(R.id.bar_loading_classify);
-		
-		mAdapter = new FileInfoAdapter(getApplicationContext(), mItemLists, mListView);
+
+		mAdapter = new FileInfoAdapter(getApplicationContext(), mItemLists,
+				mListView);
 		mListView.setAdapter(mAdapter);
-		
+
 		mFileInfoManager = new FileInfoManager();
-		
-		if (null != mGetFileTask && mGetFileTask.getStatus() == AsyncTask.Status.RUNNING) {
-			mGetFileTask.cancel(true);
-		}else {
-			mGetFileTask = new GetFileTask();
-		}
-		mGetFileTask.execute(0);
-		
+
 		mMenuHolder = (LinearLayout) findViewById(R.id.ll_menutabs_holder);
 		mMenuBarView = findViewById(R.id.menubar_bottom);
 		mMenuBarView.setVisibility(View.GONE);
-		mMenuTabManager  = new MenuTabManager(getApplicationContext(), mMenuHolder);
+		mMenuTabManager = new MenuTabManager(getApplicationContext(),
+				mMenuHolder);
+
+		long start = System.currentTimeMillis();
+		mFileClassifyScanner = new FileClassifyScanner(getApplicationContext(),
+				Environment.getExternalStorageDirectory().getAbsoluteFile(),
+				filterType);
+		mFileClassifyScanner.setScanListener(this);
+		mFileClassifyScanner.startScan();
+		Log.d(TAG, "mFileClassifyScanner cost time = " + (System.currentTimeMillis() - start));
 	};
 
-	/** get sdcard classify files */
-	class GetFileTask extends AsyncTask<Integer, Integer, Object> {
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			mLoadingBar.setVisibility(View.VISIBLE);
-			mTipView.setVisibility(View.VISIBLE);
-			mTipView.setText("正在加载...");
-		}
-
-		@Override
-		protected Object doInBackground(Integer... params) {
-			Log.d(TAG, "GetFileTask.doInBackground>type:" + mType);
-			File[] files = Environment.getExternalStorageDirectory().getAbsoluteFile().listFiles();
-			listFile(files);
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Object result) {
-			super.onPostExecute(result);
-			Log.d(TAG, "GetFileTask.onPostExecute");
-			mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_UPDATE_UI), 500);
-		}
-
-		protected void listFile(final File[] files) {
-			if (null != files && files.length > 0) {
-				for (int i = 0; i < files.length; i++) {
-					if (files[i].isDirectory()) {
-						final int tag = i;
-						new Thread(new Runnable() {
-							@Override
-							public void run() {
-								listFile(files[tag].listFiles());
-							}
-						}).start();
-					} else {
-						String name = files[i].getName();
-						FileInfo fileInfo = null;
-						if (isSpeicFile(name)) {
-							fileInfo = mFileInfoManager.getFileInfo(getApplicationContext(), files[i]);
-							mTestList.add(fileInfo);
-						}
-					}
-				}
-			}
-		}
-
-		public boolean isSpeicFile(String name) {
-			for (String str : filterType) {
-				if (name.endsWith(str)) {
-					return true;
-				}
-			}
-			return false;
-		}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mFileClassifyScanner.cancelScan();
+		mFileClassifyScanner.setScanListener(null);
 	}
 
 	@Override
-	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+	public boolean onItemLongClick(AdapterView<?> parent, View view,
+			int position, long id) {
 		if (mAdapter.isMode(ZYConstant.MENU_MODE_EDIT)) {
 			doSelectAll();
 			return true;
-		}else {
+		} else {
 			mAdapter.changeMode(ZYConstant.MENU_MODE_EDIT);
 			updateTitleNum(1, mAdapter.getCount());
 		}
 		boolean isSelected = mAdapter.isSelected(position);
 		mAdapter.setSelected(position, !isSelected);
 		mAdapter.notifyDataSetChanged();
-		
+
 		mActionMenu = new ActionMenu(getApplicationContext());
-		mActionMenu.addItem(ActionMenu.ACTION_MENU_SEND, R.drawable.ic_action_send, R.string.menu_send);
-		mActionMenu.addItem(ActionMenu.ACTION_MENU_DELETE,R.drawable.ic_action_delete_enable,R.string.menu_delete);
-		mActionMenu.addItem(ActionMenu.ACTION_MENU_RENAME, R.drawable.ic_action_rename, R.string.menu_rename);
-		mActionMenu.addItem(ActionMenu.ACTION_MENU_INFO,R.drawable.ic_action_info,R.string.menu_info);
-		mActionMenu.addItem(ActionMenu.ACTION_MENU_SELECT, R.drawable.ic_aciton_select, R.string.select_all);
-		
+		mActionMenu.addItem(ActionMenu.ACTION_MENU_SEND,
+				R.drawable.ic_action_send, R.string.menu_send);
+		mActionMenu.addItem(ActionMenu.ACTION_MENU_DELETE,
+				R.drawable.ic_action_delete_enable, R.string.menu_delete);
+		mActionMenu.addItem(ActionMenu.ACTION_MENU_RENAME,
+				R.drawable.ic_action_rename, R.string.menu_rename);
+		mActionMenu.addItem(ActionMenu.ACTION_MENU_INFO,
+				R.drawable.ic_action_info, R.string.menu_info);
+		mActionMenu.addItem(ActionMenu.ACTION_MENU_SELECT,
+				R.drawable.ic_aciton_select, R.string.select_all);
+
 		showMenuBar(true);
 		mMenuTabManager.refreshMenus(mActionMenu);
 		mMenuTabManager.setOnMenuItemClickListener(this);
@@ -212,7 +180,8 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
 		if (mAdapter.isMode(ZYConstant.MENU_MODE_EDIT)) {
 			mAdapter.setSelected(position);
 			mAdapter.notifyDataSetChanged();
@@ -223,21 +192,23 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 			mMenuTabManager.refreshMenus(mActionMenu);
 		} else {
 			// open file
-			mFileInfoManager.openFile(getApplicationContext(), mTestList.get(position).filePath);
+			mFileInfoManager.openFile(getApplicationContext(),
+					mAdapter.getItem(position).filePath);
 		}
 	}
-	
+
 	@Override
-	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
 		// TODO Auto-generated method stub
 	}
-	
+
 	@Override
 	public void onScrollStateChanged(AbsListView view, int scrollState) {
 		if (TYPE_DOC == mType || TYPE_ARCHIVE == mType) {
 			return;
 		}
-		
+
 		switch (scrollState) {
 		case OnScrollListener.SCROLL_STATE_FLING:
 			mAdapter.setFlag(false);
@@ -253,21 +224,21 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 			break;
 		}
 	}
-	
+
 	@Override
 	public void onMenuClick(ActionMenuItem item) {
 		switch (item.getItemId()) {
 		case ActionMenu.ACTION_MENU_SEND:
-//			doTransfer();
+			// doTransfer();
 			showMenuBar(false);
 			break;
 		case ActionMenu.ACTION_MENU_DELETE:
 			List<Integer> posList = mAdapter.getSelectedItemPositions();
-//			showDeleteDialog(posList);
+			// showDeleteDialog(posList);
 			break;
 		case ActionMenu.ACTION_MENU_INFO:
 			List<FileInfo> list = mAdapter.getSelectedFileInfos();
-//			mFileInfoManager.showInfoDialog(list);
+			// mFileInfoManager.showInfoDialog(list);
 			showMenuBar(false);
 			break;
 		case ActionMenu.ACTION_MENU_SELECT:
@@ -277,58 +248,63 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 			break;
 		}
 	}
-	
+
 	/**
 	 * set menubar visible or gone
+	 * 
 	 * @param show
 	 */
-	public void showMenuBar(boolean show){
+	public void showMenuBar(boolean show) {
 		if (show) {
 			mMenuBarView.setVisibility(View.VISIBLE);
-		}else {
+		} else {
 			mMenuBarView.setVisibility(View.GONE);
 			onActionMenuDone();
 			updateTitleNum(-1, mAdapter.getCount());
 		}
 	}
-	
+
 	/**
 	 * update menu bar item icon and text color,enable or disable
 	 */
-	public void updateMenuBar(){
+	public void updateMenuBar() {
 		int selectCount = mAdapter.getSelectedItems();
 		updateTitleNum(selectCount, mAdapter.getCount());
-		
+
 		if (mAdapter.getCount() == selectCount) {
-			mActionMenu.findItem(ActionMenu.ACTION_MENU_SELECT).setTitle(R.string.unselect_all);
-		}else {
-			mActionMenu.findItem(ActionMenu.ACTION_MENU_SELECT).setTitle(R.string.select_all);
+			mActionMenu.findItem(ActionMenu.ACTION_MENU_SELECT).setTitle(
+					R.string.unselect_all);
+		} else {
+			mActionMenu.findItem(ActionMenu.ACTION_MENU_SELECT).setTitle(
+					R.string.select_all);
 		}
-		
-		if (0==selectCount) {
+
+		if (0 == selectCount) {
 			mActionMenu.findItem(ActionMenu.ACTION_MENU_SEND).setEnable(false);
-			mActionMenu.findItem(ActionMenu.ACTION_MENU_DELETE).setEnable(false);
-			mActionMenu.findItem(ActionMenu.ACTION_MENU_RENAME).setEnable(false);
+			mActionMenu.findItem(ActionMenu.ACTION_MENU_DELETE)
+					.setEnable(false);
+			mActionMenu.findItem(ActionMenu.ACTION_MENU_RENAME)
+					.setEnable(false);
 			mActionMenu.findItem(ActionMenu.ACTION_MENU_INFO).setEnable(false);
-		}else {
+		} else {
 			mActionMenu.findItem(ActionMenu.ACTION_MENU_SEND).setEnable(true);
 			mActionMenu.findItem(ActionMenu.ACTION_MENU_DELETE).setEnable(true);
 			mActionMenu.findItem(ActionMenu.ACTION_MENU_RENAME).setEnable(true);
 			mActionMenu.findItem(ActionMenu.ACTION_MENU_INFO).setEnable(true);
 		}
 	}
-	
-	//Cancle Action menu
+
+	// Cancle Action menu
 	public void onActionMenuDone() {
 		mAdapter.changeMode(ZYConstant.MENU_MODE_NORMAL);
 		mAdapter.clearSelected();
 		mAdapter.notifyDataSetChanged();
 	}
-	
+
 	/**
 	 * do select all items or unselect all items
 	 */
-	public void doSelectAll(){
+	public void doSelectAll() {
 		int selectedCount = mAdapter.getSelectedItems();
 		if (mAdapter.getCount() != selectedCount) {
 			mAdapter.selectAll(true);
@@ -339,22 +315,25 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 		mMenuTabManager.refreshMenus(mActionMenu);
 		mAdapter.notifyDataSetChanged();
 	}
-	
+
 	/**
 	 * do Tranfer files
 	 */
 	public void doTransfer() {
-		ArrayList<String> checkedList = (ArrayList<String>) mAdapter.getSelectedFilePaths();
+		ArrayList<String> checkedList = (ArrayList<String>) mAdapter
+				.getSelectedFilePaths();
 
 		// send
-		FileTransferUtil fileTransferUtil = new FileTransferUtil(ClassifyActivity.this);
+		FileTransferUtil fileTransferUtil = new FileTransferUtil(
+				ClassifyActivity.this);
 		fileTransferUtil.sendFiles(checkedList, new TransportCallback() {
 
 			@Override
 			public void onTransportSuccess() {
 				int first = mListView.getFirstVisiblePosition();
 				int last = mListView.getLastVisiblePosition();
-				List<Integer> checkedItems = mAdapter.getSelectedItemPositions();
+				List<Integer> checkedItems = mAdapter
+						.getSelectedItemPositions();
 				ArrayList<ImageView> icons = new ArrayList<ImageView>();
 				for (int id : checkedItems) {
 					if (id >= first && id <= last) {
@@ -368,7 +347,7 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 
 				if (icons.size() > 0) {
 					ImageView[] imageViews = new ImageView[0];
-//					showTransportAnimation(icons.toArray(imageViews));
+					// showTransportAnimation(icons.toArray(imageViews));
 				}
 			}
 
@@ -378,7 +357,7 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 			}
 		});
 	}
-	
+
 	@Override
 	public boolean onBackKeyPressed() {
 		if (mAdapter.isMode(ZYConstant.MENU_MODE_EDIT)) {
@@ -387,13 +366,23 @@ public class ClassifyActivity extends BaseActivity implements OnItemClickListene
 		}
 		return super.onBackKeyPressed();
 	}
-	
+
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (null != mGetFileTask && mGetFileTask.getStatus() != Status.FINISHED) {
-			mGetFileTask.cancel(true);
-			mGetFileTask = null;
-		}
+	public void onScanStart() {
+		Log.d(TAG, "onScanStart");
+		mHandler.sendEmptyMessage(MSG_SCAN_START);
+	}
+
+	@Override
+	public void onScanComplete(Vector<FileInfo> fileInfos) {
+		Log.d(TAG, "onScanComplete");
+		Message message = mHandler.obtainMessage(MSG_SCAN_COMPLETE);
+		message.obj = fileInfos;
+		message.sendToTarget();
+	}
+
+	@Override
+	public void onScanCancel() {
+		Log.d(TAG, "onScanCancel");
 	}
 }
---
+++ src/com/zhaoyan/juyou/common/FileClassifyScanner.java
@@ -0,0 +1,173 @@
+package com.zhaoyan.juyou.common;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import android.content.Context;
+
+/**
+ * Scan directory to get specified types files. The scan is implements by
+ * multiple threads.
+ * 
+ */
+public class FileClassifyScanner {
+	private String[] mFilterType;
+	private File mRootDir;
+	private Context mContext;
+	private FileClassifyScanListener mListener;
+	private FileInfoManager mFileInfoManager;
+	private Vector<FileInfo> mFileInfos;
+	private int mScanningDirCount = 0;
+	private ExecutorService mExecutorService;
+
+	private boolean mIsScanning = false;
+
+	/**
+	 * 
+	 * @param context
+	 *            ApplicationContext
+	 * @param rootDir
+	 *            the directory to scan. must be a directory or
+	 *            IllegalArgumentException will be throwed.
+	 * @param filterType
+	 *            null means all type.
+	 */
+	public FileClassifyScanner(Context context, File rootDir,
+			String[] filterType) {
+		if (!rootDir.isDirectory()) {
+			throw new IllegalArgumentException(
+					"FileClassifyScanner, rootDir must be a directory.");
+		}
+		mContext = context.getApplicationContext();
+		mFileInfoManager = new FileInfoManager();
+		mFileInfos = new Vector<FileInfo>();
+		mRootDir = rootDir;
+		mFilterType = filterType;
+	}
+
+	public void startScan() {
+		if (mIsScanning) {
+			return;
+		}
+		if (mListener != null) {
+			mListener.onScanStart();
+		}
+		mScanningDirCount = 0;
+		mExecutorService = Executors.newCachedThreadPool();
+		scanDir(mRootDir);
+	}
+
+	public boolean isScanning() {
+		return mIsScanning;
+	}
+
+	public void cancelScan() {
+		if (mIsScanning) {
+			mExecutorService.shutdown();
+			if (mListener != null) {
+				mListener.onScanCancel();
+			}
+			mIsScanning = false;
+		}
+	}
+
+	public void setScanListener(FileClassifyScanListener listener) {
+		mListener = listener;
+	}
+
+	private class ScanRunnable implements Runnable {
+		private File mDir;
+
+		public ScanRunnable(File dir) {
+			if (!dir.isDirectory()) {
+				throw new IllegalArgumentException(
+						"ScanThread, dir must be a directory.");
+			}
+			mDir = dir;
+		}
+
+		@Override
+		public void run() {
+			File[] files = mDir.listFiles();
+			if (files != null) {
+				for (File file : files) {
+					if (file.isDirectory()) {
+						scanDir(file);
+					} else {
+						classifyFile(file);
+					}
+				}
+			}
+			scanDirFinish();
+		}
+	}
+
+	private synchronized void scanDir(File dir) {
+		mScanningDirCount++;
+		mExecutorService.execute(new ScanRunnable(dir));
+	}
+
+	private void classifyFile(File file) {
+		String name = file.getName();
+		boolean isMatched = false;
+		if (mFilterType != null) {
+			for (String filter : mFilterType) {
+				if (name.endsWith(filter)) {
+					isMatched = true;
+					break;
+				}
+			}
+		} else {
+			isMatched = true;
+		}
+
+		if (isMatched) {
+			FileInfo fileInfo = mFileInfoManager.getFileInfo(mContext, file);
+			mFileInfos.add(fileInfo);
+		}
+	}
+
+	private synchronized void scanDirFinish() {
+		mScanningDirCount--;
+		if (mScanningDirCount == 0) {
+			scanDirAllFinish();
+		}
+	}
+
+	private void scanDirAllFinish() {
+		mIsScanning = false;
+		try {
+			Collections.sort(mFileInfos, FileInfo.DATE_COMPARATOR);
+		} catch (Exception e) {
+		}
+		if (mListener != null) {
+			mListener.onScanComplete(mFileInfos);
+		}
+		mExecutorService.shutdown();
+	}
+
+	public interface FileClassifyScanListener {
+		/**
+		 * Scan is started. Notice, this method runs in the same thread with the
+		 * invoker.
+		 */
+		void onScanStart();
+
+		/**
+		 * Scan is complete.Notice, this method runs in the different thread
+		 * with the invoker.
+		 * 
+		 * @param fileInfos
+		 */
+		void onScanComplete(Vector<FileInfo> fileInfos);
+
+		/**
+		 * Scan is canceled when scanning. this method runs in the same thread
+		 * with the invoker.
+		 */
+		void onScanCancel();
+	}
+}
