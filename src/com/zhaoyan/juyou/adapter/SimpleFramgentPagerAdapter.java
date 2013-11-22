6204198ff1d9b620f15d63d3e2cf0ffa85464afe
alex_liang <liangxiaofei1100@163.com>
2013-11-22 14:09:59 +0800

    20131122 fix memory leak of MainActivity when activity is killed and recreat.

--- src/com/zhaoyan/juyou/adapter/SimpleFramgentPagerAdapter.java
+++ src/com/zhaoyan/juyou/adapter/SimpleFramgentPagerAdapter.java
@@ -1,31 +1,158 @@
 package com.zhaoyan.juyou.adapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class SimpleFramgentPagerAdapter extends FragmentPagerAdapter {
-	List<Fragment> mFragments = new ArrayList<Fragment>();
+/**
+ * Add fragment with specified tag.
+ * 
+ */
+public class SimpleFramgentPagerAdapter extends PagerAdapter {
+	private static final String TAG = "SimpleFramgentPagerAdapter";
+	private static final boolean DEBUG = true;
 
-	public SimpleFramgentPagerAdapter(FragmentManager fm, List<Fragment> list) {
-		super(fm);
-		this.mFragments = list;
+	private final FragmentManager mFragmentManager;
+	private FragmentTransaction mCurTransaction = null;
+	private Fragment mCurrentPrimaryItem = null;
+	private List<Fragment> mFragments;
+	private List<String> mTags;
+
+	public SimpleFramgentPagerAdapter(FragmentManager fm,
+			List<Fragment> fragments, List<String> tags) {
+		mFragmentManager = fm;
+		mFragments = fragments;
+		mTags = tags;
 	}
 
-	@Override
+	public SimpleFramgentPagerAdapter(FragmentManager fm,
+			List<Fragment> fragments) {
+		this(fm, fragments, null);
+	}
+
+	/**
+	 * Return the Fragment associated with a specified position.
+	 */
 	public Fragment getItem(int position) {
 		return mFragments.get(position);
-	}
-
-	public void addItem(Fragment fragment) {
-		mFragments.add(fragment);
 	}
 
 	@Override
 	public int getCount() {
 		return mFragments.size();
+	}
+
+	@Override
+	public void startUpdate(ViewGroup container) {
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		if (mCurTransaction == null) {
+			mCurTransaction = mFragmentManager.beginTransaction();
+		}
+
+		// Do we already have this fragment?
+		String name = makeFragmentName(container.getId(), position);
+		Fragment fragment = mFragmentManager.findFragmentByTag(name);
+		if (fragment != null) {
+			if (DEBUG)
+				Log.v(TAG, "Attaching item #" + position + ": f=" + fragment);
+			mCurTransaction.attach(fragment);
+		} else {
+			fragment = getItem(position);
+			if (DEBUG)
+				Log.v(TAG, "Adding item #" + position + ": f=" + fragment);
+			mCurTransaction.add(container.getId(), fragment, name);
+		}
+		if (fragment != mCurrentPrimaryItem) {
+			fragment.setMenuVisibility(false);
+			fragment.setUserVisibleHint(false);
+		}
+
+		return fragment;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		if (mCurTransaction == null) {
+			mCurTransaction = mFragmentManager.beginTransaction();
+		}
+		if (DEBUG)
+			Log.v(TAG, "Detaching item #" + getItemId(position) + ": f="
+					+ object + " v=" + ((Fragment) object).getView());
+		mCurTransaction.detach((Fragment) object);
+	}
+
+	@Override
+	public void setPrimaryItem(ViewGroup container, int position, Object object) {
+		Fragment fragment = (Fragment) object;
+		if (fragment != mCurrentPrimaryItem) {
+			if (mCurrentPrimaryItem != null) {
+				mCurrentPrimaryItem.setMenuVisibility(false);
+				mCurrentPrimaryItem.setUserVisibleHint(false);
+			}
+			if (fragment != null) {
+				fragment.setMenuVisibility(true);
+				fragment.setUserVisibleHint(true);
+			}
+			mCurrentPrimaryItem = fragment;
+		}
+	}
+
+	@Override
+	public void finishUpdate(ViewGroup container) {
+		if (mCurTransaction != null) {
+			mCurTransaction.commitAllowingStateLoss();
+			mCurTransaction = null;
+			mFragmentManager.executePendingTransactions();
+		}
+	}
+
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return ((Fragment) object).getView() == view;
+	}
+
+	@Override
+	public Parcelable saveState() {
+		return null;
+	}
+
+	@Override
+	public void restoreState(Parcelable state, ClassLoader loader) {
+	}
+
+	/**
+	 * Return a unique identifier for the item at the given position.
+	 * 
+	 * <p>
+	 * The default implementation returns the given position. Subclasses should
+	 * override this method if the positions of items can change.
+	 * </p>
+	 * 
+	 * @param position
+	 *            Position within this adapter
+	 * @return Unique identifier for the item at position
+	 */
+	public long getItemId(int position) {
+		return position;
+	}
+
+	private String makeFragmentName(int viewId, int id) {
+		String tag;
+		if (mTags != null) {
+			tag = mTags.get(id);
+		} else {
+			tag = "android:switcher:" + viewId + ":" + id;
+		}
+		return tag;
 	}
 }
