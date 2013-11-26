c8ee1cedfef0be925c06b6bae6cf4870571e8728
alex_liang <liangxiaofei1100@163.com>
2013-11-26 09:13:47 +0800

    20131126 Modify MainActivity fragments title.

--- src/com/zhaoyan/juyou/fragment/GuangChangFragment.java
+++ src/com/zhaoyan/juyou/fragment/GuangChangFragment.java
@@ -1,7 +1,6 @@
 package com.zhaoyan.juyou.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +8,13 @@ import android.view.ViewGroup;
 import com.zhaoyan.juyou.R;
 
 
-public class GuangChangFragment extends Fragment {
+public class GuangChangFragment extends BaseFragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View viweRoot = inflater.inflate(R.layout.guangchang_fragment, container, false);
+		initTitle(viweRoot, R.string.guang_chang);
 		return viweRoot;
 	}
 }
